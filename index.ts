e056d8be44800415ea9c16ed715a5be5c052c000
rneto <rneto@outlook.es>
2020-06-24 14:22:36 +0200

    fix: Solve user syncronize method using bug

--- index.ts
+++ index.ts
@@ -8,4 +8,4 @@ appDiv.innerHTML = `<h1>TypeScript Decorators Demo</h1>`;
 appDiv.innerHTML += `Reference code for <a href='https://rneto.es/blog/decoradores-typescript/' target='_blank'>Decorators in TypeScript</a>.`;
 
 let user = new User('Rafael');
-user.synchronize();
+user.synchronize('source');
